c8da474b6786fa14ab52099ceb82a8a2fef09cd0
Moussa camara <msa.camara@gmail.com>
2017-12-12 14:56:17 +0100

    test

---
+++ src/IKNSA - Copie/BlogBundle/Form/PostType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace IKNSA\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;    
+
+class PostType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('summary')->add('content',TextareaType::class)->add('file');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'IKNSA\BlogBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'iknsa_blogbundle_post';
+    }
+
+
+}
